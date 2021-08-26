99211cf2ef64ab884d77c6bdad74f8e7e38108fd
dtblackk <deeptoneblack@gmail.com>
2021-08-26 20:07:04 +0300

    Acıklama satırı

--- hangman.py
+++ hangman.py
@@ -1,50 +1,67 @@
 import random
 import pictures
 import words
+
 print(" 1 : Hayvanlar\n 2 : Sehirler \n 3 : Meyveler \n 4 : Sebzeler")
+
 hngmn_rsmler = pictures.Hangman_resimler
-def kategori(indx):
+
+def kategori(indx):    # Kategori Seçimi İçin Fonksiyon
     if   indx == "1":
-        kelime_listesi = words.Hayvanlar
+        lst = words.Hayvanlar
     elif indx == "2":
-        kelime_listesi = words.Sehirler
+        lst = words.Sehirler
     elif indx == "3":
-        kelime_listesi = words.Meyveler
+        lst = words.Meyveler
     else: 
-        kelime_listesi = words.Sebzeler
-    return kelime_listesi
-tercih = input("Lütfen Kategori Seçimi Yapınız : ")
-print(tercih)
+        lst = words.Sebzeler
+    return lst
+
+tercih = input("Lütfen Kategori Seçimi Yapınız : ") # Kategori için kullanıcı girdisi alıyor.
+
 while tercih not in ["1","2","3","4"]:  
-    print("Lütfen Sadece 1,2,3 veya 4 rakamını tuşlayın ! ")
-    tercih = input("Lütfen Kategori Seçimi Yapınız : ")
+    print("Lütfen Sadece 1,2,3 veya 4 rakamını tuşlayın ! ")  # Kullanıcının kategori numarası haricinde birsey
+    tercih = input("Lütfen Kategori Seçimi Yapınız : ")       # yazması durumunda tekrar sorgulama yapıyor.
+
 secilen_kelime = random.choice(kategori(tercih)).lower() # Kelime seçip tüm harfleri kücültüyor.      
-hak = 6
+
+hak = 6 # Kullanıcının tahmin hakkı.
+
 görüntü = "_" * len(secilen_kelime) # Kelimenin uzunluguna göre _ koyuyor.
-for x in range(0,len(secilen_kelime)-1):
-    while hak > 0:
+
+for x in range(0,len(secilen_kelime)-1):   
+    
+    while hak > 0:       
         print(hngmn_rsmler[hak])
         tahmin = input("Bir harf tahmin et : ").lower() # Harf alıp kücültüyor.
+
         while len(tahmin) >= 2 or tahmin.isalpha() == False:
-            print("Lütfen sadece tek bir harf girisi yapınız !")
-            tahmin = input("Bir harf tahmin et : ").lower()
-        n = -1
-        k = 0
+            print("Lütfen sadece tek bir harf girisi yapınız !")  # Kullanıcının tek harf harici birsey yazması
+            tahmin = input("Bir harf tahmin et : ").lower()       # durumunda tekrar sorgulama yapıyor.
+       
+        n = -1  # Doğru tahmin edilen harfleri eklemek icin degisken.
+        k = 0   # Dogru tahmin durumunda hakkın gitmememesi icin degisken.
+ 
         for harf in secilen_kelime :
             n +=1
+ 
             if harf == tahmin:
                 görüntü = görüntü[:n] + tahmin + görüntü[n+1:] # Kelime icinde gezinip harf ile tahminin aynı oldugu noktalarda görüntümüzü degistiriyor.
                 k += 1
+ 
                 if görüntü == secilen_kelime:
-                    print("\nTebrikler, Cevabı Buldunuz.") # Tüm harfler dogru bilinirse tebrik edip döngüden cıkarıyor.
-                    hak = -1
+                    print("\nTebrikler, Cevabı Buldunuz.") # Tüm harfler dogru bilinirse tebrik edip
+                    hak = -1                               # döngüden cıkarıyor.
             else:
                 continue
-        print("\n" + "    " + görüntü.upper())
+ 
+        print("\n" + "    " + görüntü.upper() + "\n")
+ 
         if k > 0 :
-            continue
-        else: 
-            hak -= 1
+            continue    # Tahmin doğruysa devam ediliyor.
+        else:           
+            hak -= 1    # Yanlışsa hakkımız bir azalıyor.
+
 if hak == 0:
     print(hngmn_rsmler[0])
     print("Malesef Bilemedin :( " + "\n" + "Doğru Cevap -->  " + secilen_kelime.upper())  # Haklar tükenirse doğru cevabı söyleyip bitiriyor.
